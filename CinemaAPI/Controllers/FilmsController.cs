46d7f6c6323860345787c2a4a88e94b8ec2179f4
Axel3 <zabi-zabi-zabi@rambler.ru>
2019-04-21 16:08:38 +0300

    AddSomeController2

---
+++ CinemaAPI/Controllers/FilmsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CinemaAPI.Controllers
+{
+    public class FilmsController : ApiController
+    {
+        private readonly IFilmServices _filmServices;
+
+        public FilmsController()
+        {
+            _filmServices = new FilmServices();
+        }
+
+        [HttpGet]
+
+        public IHttpActionResult GetAll()
+        {
+            var films = _filmServices.GetList();  //where GetList Create in Film services
+            return Ok(films);
+        }
+
+        [HttpGet]
+
+        public IHttpActionResult GetFilmById(int id)
+        {
+            var onefilm = _filmServices.FilmById(); //  Look like GetList().FirstOrDefault(x => x.Id == id)
+            return Ok(onefilm);
+        }
+
+        [HttpPost]
+
+
+        public IHttpActionResult AddFilm([FromBody] Film film)  //Film from models or froom EF
+        {
+
+        }
+
+
+        [HttpDelete]
+
+        public IHttpActionResult DeleteFilm(int id)
+        {
+            if (!ModelState.IsValid)
+                return (BadRequest(ModelState));
+
+            _filmServices.Delete(id);
+
+            return Ok();
+        }
+
+    }
+}
---
+++ CinemaAPI/Controllers/SessionsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CinemaAPI.Controllers
+{
+    public class SessionsController : ApiController
+    {
+        private readonly ISessionServices _sessionServices;
+
+        public SessionsController()
+        {
+            _sessionServices = new SessionServices();
+        }
+
+        [HttpGet]
+
+        public IHttpActionResult GetAll()
+        {
+            var sessions = _sessionServices.GetList();  
+            return Ok(sessions);
+        }
+
+        [HttpGet]
+
+        public IHttpActionResult GetSessionById(int id)
+        {
+            var onesession = _sessionServices.SessionById(); 
+            return Ok(onesession);
+        }
+    }
+}
